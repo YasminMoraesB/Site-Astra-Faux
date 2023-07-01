12249f2a13202e7b2b6a2a7a1e0abf811cc7e2c7
LarissaSL <larissa.silvaedge@gmail.com>
2023-07-01 14:40:02 -0300

    Criacao dos paineis
    criacao dos paineis novidades, produtos e usuarios

---
+++ Astra Faux/painel_usuarios.php
@@ -0,0 +1,68 @@
+<?php
+
+include ('conexao.php');
+
+
+
+    if(!isset($_SESSION)){
+        session_start();		
+        }
+                
+        if (!isset($_SESSION['username'])){
+            die ();
+        } else {
+                    echo "<p style = 'color:rgb(132, 14, 201)'> Usuário Logado: ";
+                    echo $_SESSION['username'];
+                    echo '<p><a href="logoff.php"> Logout</a></p>';
+                    
+    }     
+        
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <!--Bootstrap-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link href="style/cadastro.css"rel="stylesheet">
+    <link href="style/Fonts.css" rel="stylesheet">
+ 
+    
+    <title>Painel ADM</title>
+    
+</head>
+<body>
+<header>
+        <div id="title">
+            <h1>Astra Faux</h1>
+        </div>
+        <!--Páginas principais de navegação-->
+
+        <ul>
+            <a href="painel_adm.php"><li>Painel</li></a>
+            <a href="painel_usuarios.php"><li>Usuario</li></a>
+            <a href="painel_produtos.php"><li>Produtos</li></a>
+            <a href="painel_novidades.php"><li>Novidades</li></a>
+        </ul>
+    </header>
+
+    <!-- Opcoes do que o ADM pode fazer -->
+
+    <img src="./style/Components/images/logo.png" class="rounded mx-auto d-block" alt="Logo astra faux">
+
+    <a class="btn btn-primary" href="cadastro_usuario.html" role="button">Cadastrar usuario</a>
+    <a class="btn btn-primary" href="consultar_usuario.php" role="button">Consultar usuario</a>
+    <a class="btn btn-primary" href="consultar_usuario.php" role="button">Alterar usuario</a>
+    <a class="btn btn-primary" href="consultar_usuario.php" role="button">Excluir usuario</a>
+    
+
+    
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js" integrity="sha384-fbbOQedDUMZZ5KreZpsbe1LCZPVmfTnH7ois6mU1QK+m14rQ1l2bGBq41eYeM/fS" crossorigin="anonymous"></script>
+</body>
+</html>
+
+
+
