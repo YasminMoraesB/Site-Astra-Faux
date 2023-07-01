2edc2005e37c1ad5d0055098c5b0f52948864273
LarissaSL <larissa.silvaedge@gmail.com>
2023-07-01 14:41:36 -0300

    criacao do cadastro
    criacao do cadastro de novidade e produto

---
+++ Astra Faux/cadastro_produto.php
@@ -0,0 +1,68 @@
+<?php
+
+include ('conexao.php');
+
+
+
+    if(!isset($_SESSION)){
+        session_start();		
+        }
+                
+        if (!isset($_SESSION['username'])){
+            die ();
+        } else {
+                    echo "<p style = 'color:rgb(132, 14, 201)'> Usuário Logado: ";
+                    echo $_SESSION['username'];
+                    echo '<p><a href="logoff.php"> Logout</a></p>';
+                    
+    }     
+        
+?>
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <!--Bootstrap-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link href="style/cadastro.css"rel="stylesheet">
+    <link href="style/Fonts.css" rel="stylesheet">
+ 
+    
+    <title>Cadastro</title>
+    
+</head>
+<body>
+<header>
+        <div id="title">
+            <h1>Astra Faux</h1>
+        </div>
+        <!--Páginas principais de navegação-->
+
+        <ul>
+            <a href="index.php"><li>Inicio</li></a>
+            <a href="sobre.php"><li>Sobre</li></a>
+            <a href="#"><li>Produtos</li></a>
+            <a href="#"><li>Novidades</li></a>
+        </ul>
+    </header>
+
+    <!--formulário da página que irei estilziar com bootstrap -->
+
+    <section>
+        <div>
+            <h4>Criar cadastro produto</h4>
+            <!--Inicio do formulário-->
+            <form class="row g-3" action="salvarCadastro_produto.php" method="post">
+
+                
+              </form>
+        
+        <!--Fim do formulário-->
+    </section>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js" integrity="sha384-fbbOQedDUMZZ5KreZpsbe1LCZPVmfTnH7ois6mU1QK+m14rQ1l2bGBq41eYeM/fS" crossorigin="anonymous"></script>
+</body>
+</html>
