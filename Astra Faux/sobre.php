08be6ca14717af1478439b8e98d1d1e574564782
YasminMoraesB <101373507+YasminMoraesB@users.noreply.github.com>
2023-07-01 20:01:23 -0300

    update sobre.php
    permitindo que usuarios logados e deslogados acessem a pagina sobre

--- Astra Faux/sobre.php
+++ Astra Faux/sobre.php
@@ -1,16 +1,3 @@
-<?php 
-    if(!isset($_SESSION)){
-        session_start();		
-    }
-                
-    if (!isset($_SESSION['nome'])){
-        die ();
-    } else {
-        echo "<p style = 'color:white'> Usuário Logado: ";
-                    echo $_SESSION['nome'];
-                    echo '<p><a href="logoff.php"> Logout</a></p>';
-    }     
-?>
 
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -27,13 +14,26 @@
         <div id="title">
             <h1>Astra Faux</h1>
         </div>
+        <script>
+            var timeoutID;
+
+            function resetTimer() {
+                clearTimeout(timeoutID);
+                timeoutID = setTimeout(function() {
+                window.location.href = "index.php";
+                }, 120000); // Redireciona para index.php após 2 minutos (120 segundos) de inatividade
+            }
+
+            document.addEventListener("mousemove", resetTimer);
+            document.addEventListener("keydown", resetTimer);
+        </script>
         <!--Páginas principais de navegação-->
 
         <ul>
             <a href="index.php"><li>Inicio</li></a>
             <a href="sobre.php"><li>Sobre</li></a>
-            <a href="#"><li>Produtos</li></a>
-            <a href="#"><li>Novidades</li></a>
+            <a href="produtos.php"><li>Produtos</li></a>
+            <a href="novidades.php"><li>Novidades</li></a>
         </ul>
     </header>
 
@@ -76,3 +76,16 @@
       </div>
 </body>
 </html>
+<?php 
+    if(!isset($_SESSION)){
+        session_start();		
+    }
+                
+    if (!isset($_SESSION['nome'])){
+        die ();
+    } else {
+        echo "<p style = 'color:white'> Usuário Logado: ";
+                    echo $_SESSION['nome'];
+                    echo '<p><a href="logoff.php"> Logout</a></p>';
+    }     
+?>
