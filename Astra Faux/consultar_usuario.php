280e531e9f50a6761b561adaabb582375692d41d
YasminMoraesB <101373507+YasminMoraesB@users.noreply.github.com>
2023-07-01 18:23:23 -0300

    Update consultar_usuario.php
    atualizacao do consultar usuario

--- Astra Faux/consultar_usuario.php
+++ Astra Faux/consultar_usuario.php
@@ -35,7 +35,8 @@ include ('conexao.php');
     <header>
         <br><br>
         <h1 class='ml-3'> Consultar Usuários</h1>
-        <p class='ml-3'> Bem vindo a tela de consultar usuários, aqui você consegue visualizar os dados dos usuários, Editar e Deletar, basta clicar nas opções na coluna de Ações</p>
+        <p class='ml-3'> I- Para uma busca por nome <b style = "color:rgb(132, 14, 201)">digite o nome da pessoa</b> na barra de busca e <b style = "color:rgb(132, 14, 201)">clique em pesquisar</b></p>
+        <p class='ml-3'> II- Para mostrar todos os registros <b style = "color:rgb(132, 14, 201)">deixe a barra de busca vazia e clique em pesquisar</b></p>
     </header>
 
     <form action="">    
@@ -47,12 +48,56 @@ include ('conexao.php');
 
         include("conexao.php");
 
-        $sqli = "SELECT * FROM usuario";
-        $resultado = $mysqli->query($sqli) or die ("Erro ao consultar dados");
-        $qtd = $resultado->num_rows;
+
+        if(!isset($_GET ['busca'])){
+            $sqli = "SELECT * FROM usuario";
+            $resultado = $mysqli->query($sqli) or die ("Erro ao consultar dados". $mysqli -> error);
+        } else {
+
+                   // Por questão de segurança e para evitar a injeção no banco de dados é feito esse comando real_escape_string
+
+            $busca = $mysqli ->real_escape_string ($_GET['busca']);
+            $sqlBusca = "SELECT * FROM usuario WHERE nome LIKE '%$busca%'";
+            $resultado = $mysqli->query($sqlBusca) or die ("Erro ao consultar dados". $mysqli -> error);
+
+            if ($resultado ->num_rows ==0 ){
+                echo "<td>Nenhum resultado encontrado</td>";
+            }else {    
+                while ($dados = $resultado->fetch_assoc()){
+                
+                
+                echo "<tr>";
+                echo "<td>".$dados['id']. "</td>";
+                echo "<td>".$dados['nome']. "</td>";
+                echo "<td>".$dados['senha']. "</td>";
+                echo "<td>".$dados['data_de_nascimento']. "</td>";
+                echo "<td>".$dados['email']. "</td>";
+                echo "<td>".$dados['endereco']. "</td>";
+                echo "<td>".$dados['complemento']. "</td>";
+                echo "<td>".$dados['cidade']. "</td>";
+                echo "<td>".$dados['estado']. "</td>";
+                echo "<td>".$dados['cep']. "</td>";
+                
+                ?>
+
+                <td> 
+                    <a class='btn btn-primary' href='alterar_usuario.php?id=$row[id]'>Editar</a>
+                    <a class='btn btn-danger' href='excluir_usuario.php?id=$row[id]'>Excluir</a>
+                </tr>
+                <?php
 
 
-        if ($qtd > 0){
+                }
+
+            }
+            
+        }
+
+        echo "</table>";
+        
+
+
+
             //Criando o nome de cada Coluna, usando Bootstrap junto
             echo "<table class = 'table table-striped table-bordered '>";
             echo "<thead class='thead-dark'>";
@@ -73,37 +118,12 @@ include ('conexao.php');
 
             echo "</thead>";  
 
+
+
             //Obtendo os dados por meio de um loop
-            while ($row = mysqli_fetch_array ($resultado)){
-                
-                
-                echo "<tr>";
-                echo "<td>".$row['id']. "</td>";
-                echo "<td>".$row['nome']. "</td>";
-                echo "<td>".$row['senha']. "</td>";
-                echo "<td>".$row['data_de_nascimento']. "</td>";
-                echo "<td>".$row['email']. "</td>";
-                echo "<td>".$row['endereco']. "</td>";
-                echo "<td>".$row['complemento']. "</td>";
-                echo "<td>".$row['cidade']. "</td>";
-                echo "<td>".$row['estado']. "</td>";
-                echo "<td>".$row['cep']. "</td>";
-                echo "<td> 
-                        <a class='btn btn-primary' href='alterar_usuario.php?id=$row[id]'>Editar</a>
-                        </a>
-                        <a class='btn btn-danger' href='excluir_usuario.php?id=$row[id]'>Excluir</a>
-                        </a>";
-                echo "</tr>";
-
-            }
-
-            echo "</table>";
            
         //Caso não tenha dados cadastros entra aqui
         
-        }else{
-            echo "<p class='alert alert-danger'>Não existe nenhum registro no Banco de Dados!</p>";
-        }
 
     ?>
     
