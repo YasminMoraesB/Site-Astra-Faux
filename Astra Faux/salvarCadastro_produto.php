a9ad6d202969a16d04705776a235fac1b91b6ab5
LarissaSL <larissa.silvaedge@gmail.com>
2023-07-01 14:40:27 -0300

    criacao
    salvar os cadastros do produtos e novidades

---
+++ Astra Faux/salvarCadastro_produto.php
@@ -0,0 +1,20 @@
+<?php
+
+include ('conexao.php');
+
+
+
+    if(!isset($_SESSION)){
+        session_start();		
+        }
+                
+        if (!isset($_SESSION['username'])){
+            die ();
+        } else {
+                    echo "<p style = 'color:rgb(132, 14, 201)'> Usuário Logado: ";
+                    echo $_SESSION['username'];
+                    echo '<p><a href="logoff.php"> Logout</a></p>';
+                    
+    }     
+        
+?>
