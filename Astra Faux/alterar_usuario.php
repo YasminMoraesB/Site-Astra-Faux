58744378f441e07b7337271d5e5aad37eadb3aed
LarissaSL <larissa.silvaedge@gmail.com>
2023-07-03 20:08:22 -0300

    att alterar.usuario.phop
    Coloquei o controle de sessao

--- Astra Faux/alterar_usuario.php
+++ Astra Faux/alterar_usuario.php
@@ -1,3 +1,25 @@
+<?php
+
+include ('conexao.php');
+
+
+
+    if(!isset($_SESSION)){
+        session_start();		
+        }
+                
+        if (!isset($_SESSION['username'])){
+            die ();
+        } else {
+                    echo "<p style = 'color:rgb(132, 14, 201)'> Usuário Logado: ";
+                    echo $_SESSION['username'];
+                    echo '<p><a href="logoff.php"> Logout</a></p>';
+                    
+    } 
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
