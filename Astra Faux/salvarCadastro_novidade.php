64a9d21c8ec6fe628017bacf4048bdf0c1c1ad8d
YasminMoraesB <101373507+YasminMoraesB@users.noreply.github.com>
2023-07-03 21:35:50 -0300

    Update salvarcadastro_novidade.php
    update do salvarcadastro_novidade.php

--- Astra Faux/salvarCadastro_novidade.php
+++ Astra Faux/salvarCadastro_novidade.php
@@ -18,3 +18,35 @@ include ('conexao.php');
     }     
         
 ?>
+
+<?php
+
+        //Pegando as informacoes do forms e colocando em variaveis
+        include("conexao.php");
+
+        $titulo = $_POST["titulo"];
+        $autor = $_POST["autor"];
+        $texto = $_POST["textArea"];
+
+        
+        //Criacao do comando sql pra insercao no Banco de Dados
+        $sqli = "INSERT INTO novidade
+                    (data_da_publicacao, titulo, autor,texto)  
+                VALUES 
+                    (NOW(), '$titulo','$autor', '$texto') ";
+
+        
+        //Se o cadastro for feito com sucesso 
+        if($mysqli->query($sqli)){
+            echo "<script>alert('Novidade criada com sucesso!');";
+            echo "javascript:window.location='painel_novidades.php';</script>";
+
+
+        } else {
+            echo ("Falha na execução");
+
+        }
+
+
+
+    ?>
