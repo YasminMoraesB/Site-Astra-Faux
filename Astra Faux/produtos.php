d7740cca477ef0e630ef4433bf299bc060d5fb80
LarissaSL <larissa.silvaedge@gmail.com>
2023-07-04 02:48:27 -0300

    att produtos
    exibicao dos produtos funcionando

--- Astra Faux/produtos.php
+++ Astra Faux/produtos.php
@@ -1,8 +1,28 @@
+<?php 
+    if(!isset($_SESSION)){
+        session_start();
+        
+        if($_SESSION != NULL){
+            $nomeUsuario = $_SESSION['nome'];
+
+            echo "<p style = 'color:black'> Usuário Logado: ";
+            echo $nomeUsuario;
+            echo '<p><a href="logoff.php"> Logout</a></p>';	
+        }else{
+            $nomeUsuario = NULL;
+        }
+    }
+                
+         
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="style/produto.css"rel="stylesheet">
+    <link href="style/Fonts.css" rel="stylesheet">
     <title>Produtos</title>
 </head>
     <header>
@@ -32,22 +52,39 @@
         </ul>
     </header>
 <body>
-    
-    EM CONSTRUÇÃO...
+
+    <?php
+    include ('conexao.php');
+
+    $sqli = "SELECT * FROM produto";
+    $resultado = $mysqli->query($sqli) or die ("Erro ao consultar dados");
+
+    if($resultado->num_rows > 0){
+        while ($dados = $resultado->fetch_assoc()){
+            ?>
+            
+            <div class="produto">
+                <img class="produtoImagem" src=<?php  echo "./" . $dados["caminho_da_img"]; ?> alt="Imagem do Produto">
+                <h1 class="produtoNome"><?php echo $dados['nome']; ?></h1>
+                <p class="produtoDescricao"><?php echo $dados['descricao']; ?></p>
+                <p class="produtoPreco">R$ <?php echo $dados['valor']; ?></p>
+                
+                <!-- Se estiver logado abre uma nova aba onde os produtos da Gabi estão, se não estiver logado, vai pra aba de Login do Usuário , 
+                    futuramente da pra criar a aba de carrinhos e executar as vendas por esse site-->
+                
+                <?php if($nomeUsuario){ ?>
+                    <button class="produtoComprar"><a class="link"href="https://shopee.com.br/astrafaux?is_from_login=true" target="_blank">Comprar</a></button>
+                <?php }else{ ?>
+                    <button class="produtoComprar" onclick="window.location.href = 'login_usuario.php'">Comprar</button>
+                <?php } ?>
+
+            </div>
+            
+            <?php
+        }
+    }
+
+    ?>
+      
 </body>
 </html>
-
-
-<?php 
-    if(!isset($_SESSION)){
-        session_start();		
-    }
-                
-    if (!isset($_SESSION['nome'])){
-        die ();
-    } else {
-        echo "<p style = 'color:black'> Usuário Logado: ";
-                    echo $_SESSION['nome'];
-                    echo '<p><a href="logoff.php"> Logout</a></p>';
-    }     
-?>
